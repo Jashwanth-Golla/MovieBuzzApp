a6b8748d3120d6578b72b1714b4a02a045d6f00b
jashwanth golla <jashwanth.99088@gmail.com>
2024-01-20 23:22:06 +0530

    Added swagger support

--- MovieBuzzApp/src/main/java/com/moviebuzz/MovieBuzzApplication.java
+++ MovieBuzzApp/src/main/java/com/moviebuzz/MovieBuzzApplication.java
@@ -2,12 +2,14 @@ package com.moviebuzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class MovieBuzzAppApplication {
+@PropertySource("application.properties")
+public class MovieBuzzApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieBuzzAppApplication.class, args);
+		SpringApplication.run(MovieBuzzApplication.class, args);
 	}
 
 }
